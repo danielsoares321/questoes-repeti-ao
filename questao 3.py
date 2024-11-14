c99f901d317e29d7f11a827a753e804f7d81789e
danielsoares321 <soaresdaniel1796@gmail.com>
2024-11-14 16:13:19 -0300

    Add files via upload

---
+++ questao 3.py
@@ -0,0 +1,9 @@
+while True:
+    a = int(input("informe o valor "))
+    b = 10
+    if a>0 and a<11:
+        print(a)
+        print(a * b)
+    else:
+        print("valor incorreto")
+        break
